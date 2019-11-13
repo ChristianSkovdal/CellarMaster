fcad47bf24cc617995274287e28facc74bc30dc7
csa <christian@beerhere.dk>
2019-11-13 19:37:31 +0100

    db stuff

--- routes/users.js
+++ routes/users.js
@@ -5,33 +5,31 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
 
   try {
+    const sqlite3 = require('sqlite3');
 
-    res.send([]);
-    // const sqlite3 = require('sqlite3');
+    let db = new sqlite3.Database('./data/celLman.db', (err) => {
+      if (err) {
+        throw err;
+      }
 
-    // let db = new sqlite3.Database('./data/celLman.db', (err) => {
-    //   if (err) {
-    //     throw err;
-    //   }
+      let sql = `SELECT DISTINCT name,password FROM users`;
 
-    //   let sql = `SELECT DISTINCT name,password FROM users`;
+      db.all(sql, [], (err, rows) => {
+        if (err) {
+          throw err;
+        }
 
-    //   db.all(sql, [], (err, rows) => {
-    //     if (err) {
-    //       throw err;
-    //     }
+        let r = [];
+        rows.forEach((row) => {
+          r.push({
+            "name": row.name,
+            "password": row.password,
+          })
+        });
+        res.send(r);
+      });
 
-    //     let r = [];
-    //     rows.forEach((row) => {
-    //       r.push({
-    //         "name": row.name,
-    //         "password": row.password,
-    //       })
-    //     });
-    //     res.send(r);
-    //   });
-
-    // });
+    });
     
   } catch (error) {
     res.send(error);
